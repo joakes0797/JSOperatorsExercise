f6c0dedd28d5516e2e45c0d6e387c3812399fa94
Jill Oakes <jericho_rules@hotmail.com>
2022-04-25 12:39:43 -0400

    Complete Exercises 1, 2, and 3

---
+++ app.js
@@ -0,0 +1,46 @@
+console.log("--- Exercise 1 (commented out)---");
+// var a = 20;
+// var b = 4;
+// var add = a + b;
+// var minus = a - b;
+// var multiply = a * b;
+// var divide = a / b;
+// console.log(`addition ${add}`);
+// console.log(`subtraction ${minus}`);
+// console.log(`multiplication ${multiply}`);
+// console.log(`division ${divide}`);
+
+
+console.log("--- Exercise 2 ---");
+
+let a = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
+
+console.log(`value of a + str:  ${a + str}`);
+console.log(`value of a + str2:  ${a + str2}`);
+console.log(`value of a + isPresent:  ${a + isPresent}`);
+console.log(`value of a + firstName:  ${a + firstName}`);
+console.log(`value of a + lastName:  ${a + lastName}`);
+
+console.log("--- Exercise 3 ---");
+
+let d = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+let firstName2 = "Robin";
+let lastName2 = "Williams";
+
+console.log(`value of d == str3:  ${d == str3}`);
+console.log(`value of d === str3:  ${d === str3}`);
+console.log(`value of !isPresent2:  ${!isPresent2}`);
+console.log(`value of ("eleven" == str4 && d >= str3):  ${("eleven" == str4 && d >= str3)}`);
+console.log(`value of (!isPresent2 || isPresent2):  ${(!isPresent2 || isPresent2)}`);
+console.log(`value of 0 == false:  ${0 == false}`);
+console.log(`value of 0 === false:  ${0 === false}`);
+console.log(`value of 0 != false:  ${0 != false}`);
+console.log(`value of 0 !== false:  ${0 !== false}`);
